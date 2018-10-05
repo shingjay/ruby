a5759bd7a5aa4c92b0ac5546a69df6e0a706db86
Jay Ong <shingjay.ong@gmail.com>
2018-10-04 21:29:30 -0700

    Analyzer

---
+++ analyzer.rb
@@ -0,0 +1,60 @@
+
+
+###### branching
+#def multiply(num1, num2) 
+#    num1.to_i * num2.to_i
+#end
+
+#def divide(num1, num2) 
+#    num1.to_i / num2.to_i
+#end
+
+#def subtract(num1, num2) 
+#    num1.to_i - num2.to_i
+#end
+
+#puts "what do you want? 1. multiply 2. divide 3. subtract"
+#prompt = gets.chomp
+#puts "first num plz"
+#num1 = gets.chomp
+#puts "second num plz"
+#num2 = gets.chomp
+
+#if prompt == '1'
+#   puts "multiply #{multiply(num1, num2)}"
+#elsif prompt == '2'
+ #   puts "divide #{divide(num1, num2)}"
+#elsif prompt == '3'
+#    puts "subtract #{subtract(num1, num2)}"
+#else 
+#    puts "invalid"
+#end
+
+
+
+
+
+
+##### int, string, conditional
+#puts "first name plz"
+#first_name = gets.chomp
+#puts "last name plz"
+#last_name = gets.chomp
+
+#name = "#{first_name} #{last_name}"
+#puts "Hi #{name}"
+
+#puts "Hi #{name.reverse}"
+
+#puts "first num plz"
+#num1 = gets.chomp
+
+#puts "second num plz"
+#num2 = gets.chomp
+
+# puts num1 * num2 # this would fail bcoz string * string
+#puts num1.to_i * num2.to_i
+#puts num1.to_i - num2.to_i
+#uts num1.to_i + num2.to_i
+#puts "num1 % num2 #{num1.to_i % num2.to_i}"
+#puts "num2 % num1 #{num2.to_i % num1.to_i}"
