36a57f410c49be1aac6675224d7c59bc82cbf094
Jay Ong <shingjay.ong@gmail.com>
2018-10-09 22:08:05 -0700

    hashes

--- analyzer.rb
+++ analyzer.rb
@@ -1,52 +1,86 @@
+# style guide
+#https://github.com/airbnb/ruby
+
+## hashes // old way of doing stuff, use colon instead of rockt
+# deets = {
+#     'name' => 'jay', 'profession' => 'football player'
+# }
+
+# puts deets["profession"]
+
+# hash = {a: 1, b: 2, c:3} # a, b, c are symbols, not strings unlike the ones in deets
+# puts hash[:c]
+
+# hash[:d] = 7 # adds [d, 7] to the map
+# puts hash
+
+# hash[:name] = 'jay' # add a string to the hash
+# puts hash
+
+# hash.delete(:b) # delete key (b) from the hash
+# puts hash
+
+# numbers = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 10}
+# numbers.each { |key, value| puts "#{key} : #{value}" }
+
+# # numbers.each { |k,v| numbers.delete(k) } # removes everything from the map
+# numbers.each { |k,v| numbers.delete(k) if v > 3 } # removes only if value > 3
+# puts numbers
+
+# numbers[:d] = 5
+# numbers[:e] = 9
+
+# numbers.select { |k,v| v.odd? } # select only odd k-v pairs
+
 ## array
-a = [1,2,3,4,"hi", [0,1,2]]
-puts a.include?("hi")
-puts a.include?(0)
+#a = [1,2,3,4,"hi", [0,1,2]]
+#puts a.include?("hi")
+#puts a.include?(0)
 #puts a.reverse # reverse but without changing the actual array
 
-a.reverse! # actually changes the array
+#a.reverse! # actually changes the array
 #puts a
 
 #puts a.shuffle # shuffles, but also without changing the actual array
 
-y = (0..3).to_a # y = array of 0 to 9
-z = (0..99).to_a.shuffle! # array of random elements, and shuffle them
+#y = (0..3).to_a # y = array of 0 to 9
+#z = (0..99).to_a.shuffle! # array of random elements, and shuffle them
 
-puts y
-y << 25 # shuffle operator, puts a 25 at the end of the array
-puts y
-y.push(100)
-puts y # also similar to the shuffle operator
-y.unshift("test")
-puts y
-x = y.pop # removes something frm the array
-puts y 
-puts "x: #{x}"
-y.uniq # only shows unique elements in the array
+#puts y
+#y << 25 # shuffle operator, puts a 25 at the end of the array
+#puts y
+#y.push(100)
+#puts y # also similar to the shuffle operator
+#y.unshift("test")
+#puts y
+#x = y.pop # removes something frm the array
+#puts y 
+#puts "x: #{x}"
+#y.uniq # only shows unique elements in the array
  
 # going thru each element in y, each element will be i 
-y.each { |i| puts i }
+#y.each { |i| puts i }
 
 # for loop, something similar to the one above
-for number in y
-    puts "Hi #{number}"
-end
+#for number in y
+#    puts "Hi #{number}"
+#end
 
-names = [
-"anna", "jay"
-]
+#names = [
+#"anna", "jay"
+#]
 
 # another form:: names.each { |i| puts "Hello #{i.capitalize}"} 
-names.each do | person |
-    puts "Hello #{person.capitalize}" #capitalizes the first letter
-end
+#names.each do | person |
+ #   puts "Hello #{person.capitalize}" #capitalizes the first letter
+#end
 
-y = (0..50).to_a
+#y = (0..50).to_a
 # select -> you give it a boolean expression, if it's true, then you get the number
-y.select { |number| print "#{number} " if number.odd? }
+#y.select { |number| print "#{number} " if number.odd? }
 
-names = ["anna", "and", "jay"]
-puts names.join(' ')
+#names = ["anna", "and", "jay"]
+#puts names.join(' ')
 
 ###### branching
 #def multiply(num1, num2) 
